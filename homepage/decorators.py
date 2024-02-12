98c9300e386e6012e8d5b12b5fcbeebb410ee3bc
Dave <sololadavid4@gmail.com>
2024-02-12 15:10:17 +0100

    correct the user_required decorators

--- homepage/decorators.py
+++ homepage/decorators.py
@@ -9,3 +9,13 @@ def author_required(view_func):
     return _wrapped_view
 
 
+
+
+def user_required(view_func):
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.groups.filter(name= 'User'):
+            return HttpResponse('You are not auhtourized to view this page', request.user.username)
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+
