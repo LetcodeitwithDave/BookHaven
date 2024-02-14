9a9ba52b9b0b29c6a102f6b8eb336b31f9cd2d60
Dave <sololadavid4@gmail.com>
2024-02-14 01:58:34 +0100

    added deleteorder  and admin_order url

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('admin_header/', views.admin_header, name = 'admin header'),
     path('admin_page/', views.admin_page, name = 'admin page'),
+    path('deleteorder/<int:id>', views.deleteorder, name = 'deleteorder'),
+    path('admin_order/', views.admin_order, name = 'admin order'),
 ]
