943bc6149f977241a9fa2fb1f388ccc0fbc54fab
Dave <sololadavid4@gmail.com>
2024-02-15 20:59:19 +0100

    added the url path for django browser reaload package

--- BookHaven/urls.py
+++ BookHaven/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
-    path('', include('adminpanel.urls'))
+    path('', include('adminpanel.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 
 ]
