bf639b6c3a52d4f295eb502dffa5ce5f7c29fa2d
Dave <sololadavid4@gmail.com>
2024-02-21 19:57:35 +0100

    url route to admin contact page

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('admin_order/', views.admin_order, name = 'admin order'),
     path('admin_product/', views.admin_product, name = 'admin products'),
     path('deleteproduct/<int:id>', views.deleteproduct, name = 'deleteproduct'),
+    path('admin_user/', views.admin_users, name = 'admin user'),
+    path('admin_contact/', views.admin_contact, name = 'admin contact'),
+    path('deletecontact/<int:id>', views.contact_delete, name = 'deletecontact'),
+
 ]
