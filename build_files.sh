f2f2728bc853ad748e7e83209f31dcce62ed7ba4
Dave <sololadavid4@gmail.com>
2024-03-01 09:21:31 +0100

    configured

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-pip install -r requirements.txt
-python manage.py collectstatic
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
