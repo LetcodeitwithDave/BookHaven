a485a526d1bd82609a8c8d6e60ae7469fcd69ffb
Dave <sololadavid4@gmail.com>
2024-03-01 11:28:22 +0100

    reduced requirement

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -17,9 +17,6 @@ import dj_database_url
 import environ
 from dotenv import load_dotenv
 
-env = environ.Env()
-
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
