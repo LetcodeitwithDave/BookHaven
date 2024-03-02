3b18a8132561c19bdb3f5871977553e03c994b12
Dave <sololadavid4@gmail.com>
2024-03-02 07:46:41 +0100

    whitenoise configuration

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -20,6 +20,7 @@ from dotenv import load_dotenv
 env  = environ.Env()
 environ.Env.read_env()
 
+load_dotenv()
 
 
 
@@ -99,15 +100,14 @@ WSGI_APPLICATION = 'BookHaven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
-load_dotenv()
 # DATABASES = {
 #     'default': {
 #         'ENGINE': os.getenv('DB_ENGINE'),
@@ -120,12 +120,9 @@ load_dotenv()
 # }
 
 
-
-
-
-# DATABASES = {
-#     'default' : dj_database_url.parse(env('DATABASE_URL'))   
-# }
+DATABASES = {
+    'default' : dj_database_url.parse(env('DATABASE_URL'))   
+}
 
 
 
