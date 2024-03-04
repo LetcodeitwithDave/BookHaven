ef18ee88222e222bb195e6abdb828eefb1639b7e
Dave <sololadavid4@gmail.com>
2024-03-04 18:53:56 +0100

    post image

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
+
 
 sys.dont_write_bytecode = True
 
