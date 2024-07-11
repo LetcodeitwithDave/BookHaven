f4fefc2743a2307d3b5243b1cb17ab9895e1e662
Solola David <139209319+LetcodeitwithDave@users.noreply.github.com>
2024-07-11 19:56:25 +0100

    Update settings.py

--- BookHaven/settings.py
+++ BookHaven/settings.py
@@ -109,16 +109,16 @@ WSGI_APPLICATION = 'BookHaven.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': os.getenv('DB_ENGINE'),
+         'NAME': os.getenv('DB_NAME'),
+         'USER': os.getenv('DB_USER'),
+         'PASSWORD': os.getenv('DB_PASSWORD'),
+         'HOST': os.getenv('DB_HOST'),
+         'PORT': os.getenv('DB_PORT'),
+     }
+ }
 
 
 DATABASES = {
